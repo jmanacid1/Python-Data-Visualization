6690f6c0be7130bc2f54438339e8beb0fcf22656
Jackson <jacksonwlipfert@gmail.com>
2022-05-13 11:01:40 -0400

    uncommented

--- Working_With_API's.py
+++ Working_With_API's.py
@@ -1,10 +1,10 @@
 import requests
 print('a')
 
-#url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
-#headers = {'Accept': 'application/vnd.github.v3+json'}
-#r = requests.get(url, headers=headers) 
-#print (f"status code: {r.status_code}")
-#response_dict = r.json()
+url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers) 
+print (f"status code: {r.status_code}")
+response_dict = r.json()
 
-#print(response_dict.keys())
+print(response_dict.keys())
